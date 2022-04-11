f3ad3545a8ecf72c0eda869beb8f4fc2fb2e9644
Joncarre <joncarre@ucm.es>
2022-04-12 00:42:35 +0200

    gas limit

--- src/components/UserSection/index.js
+++ src/components/UserSection/index.js
@@ -73,7 +73,7 @@ const UserSection = () => {
         data.q_value,
         secureStorage.getItem('password'),
         data.numInstances,
-        { gasLimit: 20000000 })
+        { gasLimit: 21000000 })
       await transaction.wait()
     }
   }
@@ -89,7 +89,7 @@ const UserSection = () => {
         data.q_value,
         secureStorage.getItem('password'),
         data.numInstances,
-        { gasLimit: 20000000 })
+        { gasLimit: 21000000 })
       await transaction.wait()
     }
   }
